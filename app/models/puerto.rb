a0f66b1c8ef65e979fae30b6ea1f20c9685c5223
Alfredo Ramírez <alfredormz@gmail.com>
2013-07-06 18:01:37 -0300

    Cambios

--- app/models/puerto.rb
+++ app/models/puerto.rb
@@ -6,4 +6,11 @@ class Puerto < ActiveRecord::Base
 
   delegate :nombre, to: :rio, prefix: true
 
+  def ultima_semana
+    alturas.where(fecha: Date.today - 7..Date.today).order('fecha DESC')
+  end
+
+  def ultima_altura
+    alturas.order("fecha DESC").first
+  end
 end
